808c7e409f6655152d50c997e6c52025d4d9e1aa
mohsen khodadadzadeh <mohsenkhodadadzadeh@mohsens-macbook-pro.local>
2018-07-13 11:18:13 +0430

    Initial Commit

---
+++ strategyPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  strategyPattern
+//
+//  Created by mohsen khodadadzadeh on 7/13/18.
+//  Copyright © 2018 mohsen khodadadzadeh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
